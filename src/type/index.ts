460b94754e87b7f156372be0af85cf33f603ca63
PPetau <ppetau@craftingit.de>
2020-03-05 08:14:23 +0100

    feat(GetArguments): remove declaration of metadata

--- src/type/index.ts
+++ src/type/index.ts
@@ -11,28 +11,17 @@ export { ResponseParser } from './ResponseParser';
 
 export { Template, UrlTemplate } from './Template';
 
-const ArgumentMetadataKey = Symbol('api:argumentlist');
-
 export function GetArguments(func: Function): string[] {
-  let metadata: string[] | undefined = Reflect.getMetadata(
-    ArgumentMetadataKey,
-    func
-  );
+  // First match everything inside the function argument parens.
+  const args = func.toString().match(/^.*?\(([^)]*)\)/)?.[1] ?? '';
 
-  if (typeof metadata === 'undefined') {
-    // First match everything inside the function argument parens.
-    const args = func.toString().match(/^.*?\(([^)]*)\)/)?.[1] ?? '';
-
-    // Split the arguments string into an array comma delimited.
-    metadata = args
+  // Split the arguments string into an array comma delimited.
+  return (
+    args
       .split(',')
       // Ensure no inline comments are parsed and trim the whitespace.
       .map(arg => arg.replace(/\/\*.*\*\//, '').trim())
       // Ensure no undefined values are added.
-      .filter(arg => arg);
-
-    Reflect.defineMetadata(ArgumentMetadataKey, metadata, func);
-  }
-
-  return metadata;
+      .filter(arg => arg)
+  );
 }
